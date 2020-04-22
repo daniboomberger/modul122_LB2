7d6db34e96ffca01b57ad1c95462c6d7aef79bc9
daniboomberger <dani.boomberger@gmail.com>
2020-04-22 19:31:11 +0200

    created connection to customer ftp server, get file from customer ftp server (didn't upload configuration file because of security reasons)

---
+++ main.py
@@ -0,0 +1,27 @@
+import configuration
+import ftplib
+
+class customerFTP():
+    
+    #open connection to customer FTP
+    def __init__(self):
+        self.ftp = ftplib.FTP(configuration.CUSTOMER_HOST)
+        self.ftp.login(configuration.CUSTOMER_USER, configuration.CUSTOMER_PASSWORD)
+        self.ftp.cwd(configuration.CUSTOMER_PATH)
+
+    def getFileFromCustomerFTP(self):
+        #create local file
+        fileName  = configuration.CUSTOMER_FILE
+        localFile = open(fileName, 'wb')
+
+        #get remote file from customer FTP Server
+        self.ftp.retrbinary('RETR %s' % fileName, localFile.write)
+        
+        localFile.close()
+        self.ftp.close()
+
+
+
+if __name__ == '__main__':
+
+    customerFTP().getFileFromCustomerFTP()
